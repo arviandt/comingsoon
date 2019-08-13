b206420f1d6386ae6a8ac16110ee52843395756a
rakhaviantoni <rakha.rv@gmail.com>
2019-08-14 04:33:19 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import Arviandt from './Arviandt.png';
+import blob from './blob.png';
+import triangle from './triangle.png';
 import './App.css';
+import Tilt from 'react-tilt';
+import ParticleComponent from "./ParticleComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tiltOptions: {
+        reverse: true,  // reverse the tilt direction
+        max: 10,     // max tilt rotation (degrees)
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <ParticleComponent />
+        <img src={blob} className="blob" alt="blob" />
+        <Tilt className="bg" options={{ reverse: true, max: 20, scale: 1 }} >
+          <img src={Arviandt} className="logo" alt="Arviandt" />
+        </Tilt>
+        <div className="title">
+          <h1>Prepare Yourself,</h1>
+          <span>Our new website is on its way...</span>
+        </div>
+        <img src={triangle} className="triangle" alt="triangle" />
+      </div>
+    );
+  }
 }
 
 export default App;
