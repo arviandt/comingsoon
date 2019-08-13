61bfc06e3ab4a2f4ca9f1476c764fc0f989c9d9e
rakhaviantoni <rakha.rv@gmail.com>
2019-08-14 04:42:29 +0700

    Updates

---
+++ precache-manifest.a068da321e900f5ca0e12620d7ccff96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2dae3b8bc0d3d40767c591d4c526d70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd48bcdc5555ca62f15b",
+    "url": "/static/css/main.352a403d.chunk.css"
+  },
+  {
+    "revision": "2bf7fad45689869f4bdc",
+    "url": "/static/js/2.82811ce3.chunk.js"
+  },
+  {
+    "revision": "fd48bcdc5555ca62f15b",
+    "url": "/static/js/main.c4eb5a9d.chunk.js"
+  },
+  {
+    "revision": "9e9348ff0ce4ef89f285",
+    "url": "/static/js/runtime~main.90175c0c.js"
+  },
+  {
+    "revision": "68a7721f347cbb8b2b9a7861ec9fdba9",
+    "url": "/static/media/Arviandt.68a7721f.png"
+  },
+  {
+    "revision": "a288351dfbcf1afd8f378054e1838b16",
+    "url": "/static/media/blob.a288351d.png"
+  }
+]);
